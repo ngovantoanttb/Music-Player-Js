a64835cbec9413d144b15bb330c76452f45449f6
ngovantoanttb <ngovantoan1305y@gmail.com>
2024-09-26 23:58:21 +0700

    update

--- assets/database/songs.js
+++ assets/database/songs.js
@@ -7,43 +7,43 @@ export default {
         },
         {
             name: 'À lôi',
-            singer: '',
+            singer: 'Double2T x Masew',
             path: 'assets/music/aloi.mp3',
             image: 'assets/img/aloi.jpg'
         },
         {
             name: 'Cưới thôi em',
-            singer: '',
+            singer: 'Thái Học',
             path: 'assets/music/cuoithoiem.mp3',
             image: 'assets/img/cuoithoiem.jpg'
         },
         {
             name: 'Đi giữa trời rực rỡ',
-            singer: '',
+            singer: 'Ngô Lan Hương',
             path: 'assets/music/digiuatroirucro.mp3',
             image: 'assets/img/digiuatroirucro.jpg'
         },
         {
             name: 'Đừng làm trái tim anh đau',
-            singer: '',
+            singer: 'Sơn Tùng MTP',
             path: 'assets/music/dunglamtraitimanhdau.mp3',
             image: 'assets/img/dunglamtraitimanhdau.jpg'
         },
         {
             name: 'Em là chân ái',
-            singer: '',
+            singer: 'Duy Tuyên',
             path: 'assets/music/emlachanai.mp3',
             image: 'assets/img/emlachanai.jpg'
         },
         {
             name: 'Giang hải không độ nàng',
-            singer: '',
+            singer: 'Không xác định',
             path: 'assets/music/gianghai.mp3',
             image: 'assets/img/gianghai.jpg'
         },
         {
             name: 'Rồi ta sẽ ngắm pháo hoa cùng nhau',
-            singer: '',
+            singer: 'JUUN D x O.LEW (Duet Version)',
             path: 'assets/music/ngamphaohoa.mp3',
             image: 'assets/img/ngamphaohoa.jpg'
         }
