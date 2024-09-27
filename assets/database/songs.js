1cdf3fa1ec1c130098aeb98663a5cf3240838336
ngovantoanttb <ngovantoan1305y@gmail.com>
2024-09-28 02:10:40 +0700

    update

--- assets/database/songs.js
+++ assets/database/songs.js
@@ -46,6 +46,55 @@ export default {
             singer: 'JUUN D x O.LEW (Duet Version)',
             path: 'assets/music/ngamphaohoa.mp3',
             image: 'assets/img/ngamphaohoa.jpg'
-        }
+        },
+        {
+            name: 'Khuất lối',
+            singer: 'H Kray',
+            path: 'assets/music/song1.mp3assets/music/song1.mp3',
+            image: 'assets/img/song1.jpg'
+        },
+        {
+            name: 'Sao cũng được',
+            singer: 'Thành Đạt',
+            path: 'assets/music/song2.mp3',
+            image: 'assets/img/song2.jpg'
+        },
+        {
+            name: 'Tiếng pháo tiễn người',
+            singer: 'Hùng Quân',
+            path: 'assets/music/song4.mp3',
+            image: 'assets/img/song4.jpg'
+        },
+        {
+            name: 'Thay tôi yêu cô ấy',
+            singer: 'Thanh Hưng',
+            path: 'assets/music/song5.mp3',
+            image: 'assets/img/song5.jpg'
+        },
+        {
+            name: 'Yêu khác thương hại',
+            singer: 'Thanh Hưng',
+            path: 'assets/music/song6.mp3',
+            image: 'assets/img/song6.jpg'
+        },
+        {
+            name: 'Ai đợi mình được mãi',
+            singer: 'Thanh Hưng',
+            path: 'assets/music/song7.mp3',
+            image: 'assets/img/song7.jpg'
+        },
+        {
+            name: 'Chắc vì mình chưa tốt',
+            singer: 'Thanh Hưng',
+            path: 'assets/music/song8.mp3',
+            image: 'assets/img/song8.jpg'
+        },
     ],
 }
+
+// {
+//     name: '',
+//     singer: '',
+//     path: '',
+//     image: ''
+// },
